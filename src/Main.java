e854fe9865a05f6ea4ab5d76542f710dbd661c02
Александр <klerik.af.miass@gmail.com>
2024-12-25 19:26:52 +0500

    home-work.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,103 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Проверка связи!");
+        System.out.println("Task 1");
+
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+        double[] fractionalNumbers = {1.57, 7.654, 9.986};
+
+        int[] lamb = new int[8];
+        lamb[0] = 1;
+        lamb[1] = 2;
+        lamb[2] = 3;
+        lamb[3] = 4;
+        lamb[4] = 5;
+        lamb[5] = 6;
+        lamb[6] = 7;
+        lamb[7] = 8;
+
+        System.out.println("Task 2");
+        for (int index = 0; index < numbers.length; index++) {
+            if (index == numbers.length - 1) {
+                System.out.println(numbers[index]);
+                break;
+            }
+            System.out.print(numbers[index] + ", ");
+
+
+        }
+        for (int index = 0; index < fractionalNumbers.length; index++) {
+            if (index == fractionalNumbers.length - 1) {
+                System.out.println(fractionalNumbers[index]);
+                break;
+            }
+            System.out.print(fractionalNumbers[index] + ", ");
+
+
+        }
+        for (int index = 0; index < lamb.length; index++) {
+            if (index == lamb.length - 1) {
+                System.out.println(lamb[index]);
+                break;
+            }
+            System.out.print(lamb[index] + ", ");
+
+
+        }
+
+
+        System.out.println("Task 3");
+
+        for (int index = 2; index < 3; index = index - 1) {
+            {
+                if (index == numbers.length - 3) {
+                    System.out.println(numbers[index]);
+                    break;
+                }
+
+                System.out.print(numbers[index] + ", ");
+
+
+            }
+        }
+        for (int index = 2; index < 3; index = index - 1) {
+            {
+                if (index == fractionalNumbers.length - 3) {
+                    System.out.println(fractionalNumbers[index]);
+                    break;
+                }
+
+                System.out.print(fractionalNumbers[index] + ", ");
+
+
+            }
+        }
+        for (int index = 7; index < 8; index = index - 1) {
+            {
+                if (index == lamb.length - 8) {
+                    System.out.println(lamb[index]);
+                    break;
+                }
+
+                System.out.print(lamb[index] + ", ");
+
+
+            }
+        }
+
+        System.out.println("Task 4");
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                numbers[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(numbers));
+
+
     }
 }
